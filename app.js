4d87fdc2fa1ad4e675d8592d6204c26d10653a67
EC2 Default User <ec2-user@ip-172-31-27-10.us-east-2.compute.internal>
2020-02-17 07:29:00 +0000

    new

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var GuessNumberRouter = require("./routes/GuessNumber");
+var lab3Router = require("./routes/lab3");
 
 var app = express();
 
@@ -20,10 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('GuessNumber'));
+app.use(express.static('lab3'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/GuessNumber', GuessNumberRouter);
+app.use('/lab3', lab3Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
