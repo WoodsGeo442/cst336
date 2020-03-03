40b40327a886f86795ebc4de5df3fe3d8880fc92
WoodsGeo442 <gewoods@csumb.edu>
2020-03-02 16:24:56 -0800

    new

--- MidtermPractice/js/functions.js
+++ MidtermPractice/js/functions.js
@@ -1,66 +1,42 @@
-var keywords = ["dog"/*, "city", "cats", "park", "college"*/];
-// let randomKeywords = Math.floor(Math.random() * 5);
+// let getImages = function(){
+//     $(".flex-container").empty();
+//     if($("#number").val() == 0){
+//         return;
+//     }
+//     $.ajax({
+//         type: "GET",
+//         url: "https://dog.ceo/api/breeds/image/random/" + num,
+//         dataType: "json",
+      
+        
+//         success: function(data){
+//             console.log(data);
+//             result.message.forEach(function(link){
+//                 var img = "<div class='img-thumbnail flex-item'><img src='" + link + "'></div>";
+//                 $(".flex-container").append(img);
+//             });
+            
+            
+//         }
+//     });
+// };
 
-// let randomindex1 = Math.floor(Math.random() * 20);
-// let randomindex2 = Math.floor(Math.random() * 20);
-// let randomindex3 = Math.floor(Math.random() * 20);
-// let randomindex4 = Math.floor(Math.random() * 20);
-
-
-$(document).ready(function(){
+buttonDog.addEventListener("click", function(){
+    $(".flex-container").empty();
+    if($("#inputNum").val() == 0){
+      return;
+    }
     $.ajax({
         method: "GET",
-        url: "https://pixabay.com/api/",
+        url: "https://dog.ceo/api/breeds/image/random/" + $("#inputNum").val(),
         dataType: "json",
-        data: {
-            "key": "15447637-eedcbc2fcfe6b70ed3f3cccbb",
-            "q": keywords[0]
-            // "orientation": $("#orientation").val()
-        },
-        
-        success: function(result, status){
-            $("#first").attr('src', result.hits[randomindex1].previewURL);
-            $("#second").attr('src', result.hits[randomindex2].previewURL);
-            $("#third").attr('src', result.hits[randomindex3].previewURL);
-            $("#fourth").attr('src', result.hits[randomindex4].previewURL);
-                
-            $("#likesPic1").html(result.hits[randomindex1].likes);
-            $("#likesPic2").html(result.hits[randomindex2].likes);
-            $("#likesPic3").html(result.hits[randomindex3].likes);
-            $("#likesPic4").html(result.hits[randomindex4].likes);
-        }
-                
-    });
+        success: function(result) {
+          console.log(result);
+          result.message.forEach(function(link){
+            var img = "<div class='img-thumbnail flex-item'><img src='" + link + "'></div>";
+            $(".flex-container").append(img);
+          });
 
-    $("#searchImage").submit(function(e){
-        e.preventDefault();
-    
-        $.ajax({
-            method: "GET",
-            url: "https://pixabay.com/api/",
-            dataType: "json",
-            data:{
-                "key": "15447637-eedcbc2fcfe6b70ed3f3cccbb",
-                "q": $("#word").val(),
-                "orientation": $("#orientation").val()
-            },
-            success: function(result, status){
-                let index1 = Math.floor(Math.random() * result.hits.length);
-                let index2 = Math.floor(Math.random() * result.hits.length);
-                let index3 = Math.floor(Math.random() * result.hits.length);
-                let index4 = Math.floor(Math.random() * result.hits.length);
-                
-                $("#first").attr('src', result.hits[index1].previewURL);
-                $("#second").attr('src', result.hits[index2].previewURL);
-                $("#third").attr('src', result.hits[index3].previewURL);
-                $("#fourth").attr('src', result.hits[index4].previewURL);
-                
-                $("#likesPic1").html(result.hits[index1].likes);
-                $("#likesPic2").html(result.hits[index2].likes);
-                $("#likesPic3").html(result.hits[index3].likes);
-                $("#likesPic4").html(result.hits[index4].likes);
-            
-            }
-        });
+        }
     });
 });
