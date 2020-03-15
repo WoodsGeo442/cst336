2468b6c49cc362716a8b95171e2518017427df7f
EC2 Default User <ec2-user@ip-172-31-27-10.us-east-2.compute.internal>
2020-03-15 22:20:09 +0000

    new

--- labs/lab6/app.js
+++ labs/lab6/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+app.use(express.static('css'));
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 
@@ -16,7 +19,17 @@ app.get("/venus", function(req, res){
     res.send("This will be Venus web page!");
 });
 
-//server listener
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Running Express Server...");
+/* The handler for undefined routes */
+app.get("*", function(req, res){
+   res.send("<h2 style='color: red;'> Sorry, Page not found !</h2>"); 
 });
+
+// //server listener
+// app.listen(process.env.PORT, process.env.IP, function(){
+//     console.log("Running Express Server...");
+// });
+
+/* Start the application server */
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server has been started");
+})
