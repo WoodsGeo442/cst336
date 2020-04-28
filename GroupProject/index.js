9b879595099d5c1a487b0f47bcece3d98434cdd8
EC2 Default User <ec2-user@ip-172-31-27-10.us-east-2.compute.internal>
2020-04-28 04:27:15 +0000

    new

--- GroupProject/index.js
+++ GroupProject/index.js
@@ -1,8 +1,17 @@
 /*App Configuration*/
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
 var mysql = require('mysql');
+var session = require('express-session');
+var bcrypt = require('bcrypt');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret: 'top secret code!',
+    resave: true,
+    saveUninitialized: true
+}));
 app.set('view engine', 'ejs');
 
 /*Configure MySQL DBMS*/
@@ -10,18 +19,76 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'geowoods',
     password: 'Rockydale442',
-    database: 'quotes_db'
+    database: 'games'
 });
 connection.connect();
 
+/* Middleware */
+function isAuthenticated(req, res, next){
+    if(!req.session.authenticated) res.redirect('/login');
+    else next();
+}
+
+function checkUsername(username){
+    let stmt = 'SELECT * FROM loginInfo WHERE username=' + username;
+    return new Promise(function(resolve, reject){
+       connection.query(stmt, [username], function(error, results){
+           if(error) throw error;
+           resolve(results);
+       }); 
+    });
+}
+
+function checkPassword(password, hash){
+    return new Promise(function(resolve, reject){
+       bcrypt.compare(password, hash, function(error, result){
+          if(error) throw error;
+          resolve(result);
+       }); 
+    });
+}
+
 //routes
 app.get('/', function(req, res){
+    // var sql = 'select country from l9_author';
+    // connection.query(sql, function(error, results) {
+    //     if(error) throw error;
+    //     var arr = [];
+    //     results.forEach(function(r) {
+    //         if (!arr.includes(r.country)) {
+    //             arr.push(r.country);
+    //         }
+    //     });
+    //     res.render('home', {countries: arr});
+    // });
     res.render('home');
+});
+
+app.get('/login', function(req,res){
+    res.render('login');
+})
+
+app.post('/login', async function(req, res){
+    let isUserExist   = await checkUsername(req.body.username);
+    let hashedPasswd  = isUserExist.length > 0 ? isUserExist[0].password : '';
+    let passwordMatch = await checkPassword(req.body.password, hashedPasswd);
+    if(passwordMatch){
+        req.session.authenticated = true;
+        req.session.user = isUserExist[0].username;
+        res.redirect('login');
+    }
+    else{
+        res.render('welcome', {error: true});
+    }
 });
 
 app.get('/welcome', function(req, res){
     res.render('welcome');
 });
+
+app.get('/results', function(req, res){
+    res.render('results');
+})
 
 app.get('*', function(req, res){
     res.render('error');
--- GroupProject/sql/games.sql
+++ GroupProject/sql/games.sql
@@ -3,6 +3,7 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+-- CREATE TABLE FOR USERS
 DROP TABLE IF EXISTS `loginInfo`;
 CREATE TABLE `loginInfo` (
   `loginId` mediumint(20) NOT NULL,
@@ -32,10 +33,86 @@ INSERT INTO `loginInfo` (`loginId`, `username`, `password`) VALUES
 (19, 'Y', 'Q'),
 (20, 'R', 'T');
 
+-- CREATE TABLE FOR VIDEO GAMES
+DROP TABLE IF EXISTS `videoGames`;
+CREATE TABLE `videoGames`(
+    -- videogame_id, title, genre, platform, rating, pricing;
+    `videogame_id` mediumint(20) NOT NULL,
+    `title`    varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+    `genre`    varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+    `platform` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+    `rating`   mediumint(10) NOT NULL,
+    `pricing`  float(100) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `videoGames` (`videogame_id`, `title`, `genre`, `platform`, `rating`, `pricing`) VALUES
+(1, 'DOOM', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(2, 'Sekiro: Shadows Die Twice', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(3, 'Resident Evil 2 Remake', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(4, 'Death Stranding', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(5, 'Control', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(6, 'The Outer Worlds', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(7, 'Outer Wilds', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(8, 'Star Wars Jedi: Fallen Order', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(9, 'Apex Legends', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(10, 'The Legend of Zelda: Links Awakening', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(11, 'Gears 5', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(12, 'Astral Chain', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(13, 'Devil May Cry 5', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(14, 'Borderlands 3', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(15, 'Days Gone', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(16, 'Call of Duty: Modern Warfare', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(17, 'Metro Exodus', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(18, 'Pokémon Sword and Shield', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(19, 'Final Fantasy 14: Shadowbringers', `FPS`, `Console, PC`, `9/10`, `59.99`),
+(20, 'The Division 2', `FPS`, `Console, PC`, `9/10`, `59.99`);
+
+-- CREATE TABLE FOR GAME DEVELOPERS
+DROP TABLE IF EXISTS `gameDevelopers`;
+CREATE TABLE `gameDevelopers`(
+    -- gameDevelopers_id, company_name;
+    `gameDevelopers_id` mediumint(20) NOT NULL,
+    `company_name`      varchar(25) COLLATE utf8_unicode_ci NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `gameDevelopers` (`gameDevelopers_id`, `company_name`) VALUES
+(1, 'Nintendo'),
+(2, 'Valve Corporation'),
+(3, 'Rockstar Games'),
+(4, 'Electronic Arts'),
+(5, 'Activision Blizzard'),
+(6, 'Sony Computer Entertainment'),
+(7, 'Ubisoft'),
+(8, 'Sega Games Co. Ltd'),
+(9, 'BioWare'),
+(10, 'Square Enix Holdings Co. Ltd'),
+(11, 'Capcom Company Ltd'),
+(12, 'Bungie Inc'),
+(13, 'Microsoft Corporation'),
+(14, 'Bandai Namco Entertainment'),
+(15, 'Mojang'),
+(16, 'Epic Games'),
+(17, 'Game Freak'),
+(18, 'Insomniac Games Inc'),
+(19, 'Infinity Ward'),
+(20, 'ZeniMax Media Inc');
+
 ALTER TABLE `loginInfo`
+  ADD PRIMARY KEY (`loginId`);
+
+ALTER TABLE `videoGames`
+  ADD PRIMARY KEY (`loginId`);
+
+ALTER TABLE `gameDevelopers`
   ADD PRIMARY KEY (`loginId`);
   
 ALTER TABLE `loginInfo`
+  MODIFY `loginId` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+ALTER TABLE `videoGames`
+  MODIFY `loginId` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+ALTER TABLE `gameDevelopers`
   MODIFY `loginId` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
   
 COMMIT;
